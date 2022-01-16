b82e83cc54375325b817861376243e3f63783e44
Mike Koss <mike@mckoss.com>
2022-01-16 00:00:08 -0800

    Show variance

--- src/test-runner.ts
+++ src/test-runner.ts
@@ -41,6 +41,7 @@ for (const word of tests) {
     subset = new Set(words);
     const guessStats = analyze(dict, 1, subset);
     guess = guessStats[0].guess;
+    guesses.push(`V${guessStats[0].variance.toFixed(1)}`);
   }
 }
 
