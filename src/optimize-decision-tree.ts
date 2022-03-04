9fd3a0df504e7d7decad740ed51962d3c7316a41
Mike Koss <mike@mckoss.com>
2022-03-03 22:35:07 -0800

    Find potential sub-optimal 2nd guess nodes.

--- src/optimize-decision-tree.ts
+++ src/optimize-decision-tree.ts
@@ -49,6 +49,12 @@ class LevelStats {
     return this.numSquaredPartition / this.words;
   }
 
+  // Depth and count at depth
+  static maxDepth(stats: LevelStats[]): [number, number] {
+    const last = stats.length - 1;
+    return [last, stats[last].solves];
+  }
+
   static expectedGuesses(stats: LevelStats[]): number {
     let words = 0;
     let sum = 0;
@@ -66,41 +72,86 @@ class LevelStats {
 // represented by a child node.
 class WordleNode {
   guess: string | undefined;
-  children: Map<Clue, WordleNode>;
+  children: Map<Clue, WordleNode> | undefined;
   words: Set<string>
 
   constructor(guess: string | undefined, words: Set<string>) {
     this.guess = guess;
-    this.children = new Map();
     this.words = words;
   }
 
   // Expand the node by generating all possible clues and their corresponding
   // guesses.
-  expandNode() {
+  // This function tries several alternative of the 2nd level nodes to see
+  // if there are alternatives with fewer words at the max depth.
+  expandNode(level = 1) {
+    let numOptions = level === 2 ? 5 : 1;
     // Find the best guess for this node if we don't have one already.
     if (this.guess === undefined) {
-      const {guess, numSets} = analyze(dict, 1, this.words)[0];
+      const {guess, numSets} = analyze(dict, numOptions, this.words)[0];
       this.guess = guess;
       if (numSets === 1) {
         return;
       }
     }
 
-    // Generate all possible clues for this node.
-    const clues = wordle.allClues(this.guess, this.words);
+    this.children = this.computeChildren(level, this.guess);
+  }
+
+  computeChildren(level: number, guess: string): Map<Clue, WordleNode> {
+    const children = new Map<Clue, WordleNode>();
+
+    const clues = wordle.allClues(guess, this.words);
     for (const [clue, words] of clues) {
       if (clue === '!!!!!') {
         continue;
       }
       const child = new WordleNode(undefined, words);
-      this.children.set(clue, child);
-      child.expandNode();
+      children.set(clue, child);
+      child.expandNode(level + 1);
     }
+
+    return children;
   }
 
   get inSolution(): boolean {
     return this.words.has(this.guess!);
+  }
+
+  get depth(): number {
+    let depth = 1;
+
+    if (this.children === undefined) {
+      return depth;
+    }
+
+    for (const child of this.children.values()) {
+      depth = Math.max(depth, child.depth + 1);
+    }
+
+    return depth;
+  }
+
+  // Count number of solutions at a specific depth from this node.
+  countDepth(depth: number): number {
+    if (depth <= 0) {
+      return 0;
+    }
+    if (depth === 1) {
+      return this.inSolution ? 1 : 0;
+    }
+
+    if (this.children === undefined) {
+      return 0;
+    }
+
+    let count = 0;
+
+    for (let child of this.children.values()) {
+      count += child.countDepth(depth - 1);
+    }
+
+    return count;
   }
 
   getNodeStats(level = 1, stats?: LevelStats[]) : LevelStats[] {
@@ -116,12 +167,14 @@ class WordleNode {
       stats[level].solves++;
     }
 
-    for (const child of this.children.values()) {
-      child.getNodeStats(level + 1, stats);
-      stats[level].words += child.words.size;
-      stats[level].maxPartition = Math.max(stats[level].maxPartition, child.words.size);
-      stats[level].countPartition++;
-      stats[level].numSquaredPartition += child.words.size ** 2;
+    if (this.children !== undefined) {
+      for (const child of this.children.values()) {
+        child.getNodeStats(level + 1, stats);
+        stats[level].words += child.words.size;
+        stats[level].maxPartition = Math.max(stats[level].maxPartition, child.words.size);
+        stats[level].countPartition++;
+        stats[level].numSquaredPartition += child.words.size ** 2;
+      }
     }
 
     return stats;
@@ -137,3 +190,12 @@ for (let i = 1; i < stats.length; i++) {
 }
 
 console.log(`Expected guesses: ${LevelStats.expectedGuesses(stats).toFixed(2)}`);
+const [level, count] = LevelStats.maxDepth(stats);
+console.log(`Max depth: ${count} words at level ${level}`);
+
+console.log("Clues for 2nd guess for max-depth (5) nodes:");
+for (const [clue, child] of root.children!.entries()) {
+  if (child.depth === 4) {
+    console.log(`${clue}: ${child.countDepth(4)}.`);
+  }
+}
